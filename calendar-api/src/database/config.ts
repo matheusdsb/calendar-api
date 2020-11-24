8a631074bf39e683b820d211602b23104f9055b8
Matheus Sabino <matheusdsb@gmail.com>
2020-11-23 22:26:58 -0300

    updating database config

--- calendar-api/src/database/config.ts
+++ calendar-api/src/database/config.ts
@@ -1,21 +1,18 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { DailyEvent } from "./entities/daily-event.entity";
+import { Event } from './entities/event.entity';
 
 class DatabaseConfig {
     static getTypeOrmConfig() : TypeOrmModuleOptions  {
         return {
             type: 'postgres',
-            host: process.env.DATABASE_HOST,
-            port: parseInt(process.env.DATABASE_PORT),
-            username: process.env.DATABASE_USER,
-            password: process.env.DATABASE_PASSWORD,
-            database: process.env.DATABASE_NAME,
-            entities: [],
+            host: process.env.DATABASE_HOST,            
+            port: parseInt(process.env.DATABASE_PORT),            
+            username: process.env.DATABASE_USER,            
+            password: process.env.DATABASE_PASSWORD,            
+            database: process.env.DATABASE_NAME,            
+            entities: [ Event, DailyEvent ],
             synchronize: false,
-            migrationsTableName: 'tb_migration',
-            migrations: ['database/migrations/*.ts'],            
-            cli: {
-                migrationsDir: 'database/migrations'
-            },
             maxQueryExecutionTime: 10000
         }
     }
