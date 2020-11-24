2a553386455af53596a5303d8da9f85b90df513f
Matheus Sabino <matheusdsb@gmail.com>
2020-11-23 22:27:39 -0300

    generating testing actions

--- calendar-api/src/controllers/events.controller.ts
+++ calendar-api/src/controllers/events.controller.ts
@@ -1,11 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { getConnection, getCustomRepository, getManager } from 'typeorm';
+import { EventRepository } from '../database/repositories/event.repository';
+import { Event } from '../database/entities/event.entity';
+import { Response } from 'express';
+import { DailyEvent } from '../database/entities/daily-event.entity';
+import { DailyEventRepository } from '../database/repositories/daily-event.repository';
+import { DailyEventBuilder } from '../database/builders/daily-event.builder';
 
 @Controller('events')
 export class EventsController {
   constructor() {}
 
   @Get()
-  getHello(): string {
-    return 'hello events';
+  async list(@Res() res: Response): Promise<any> {
+
+    const repo = getCustomRepository(EventRepository);
+    const list = await repo.find();
+    return res.status(HttpStatus.OK).json(list);
+  }
+
+  @Get('create')
+  async create(@Res() res: Response) {
+
+    let event = new Event();
+    event.name = 'daily event';
+    event.startDateTime = new Date();
+    event.durationTime = "15:30:00";
+
+    let daily = new DailyEvent();
+    daily.event = event;
+
+    daily = await DailyEventBuilder.create(getConnection(), daily);
+
+    return res.status(HttpStatus.CREATED).json(daily);
   }
 }
